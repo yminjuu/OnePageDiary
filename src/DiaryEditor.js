c5f680e28953e8f659045012cc741274e3f7d3d1
yminjuu <ymj0580@naver.com>
2024-02-08 17:30:38 +0900

    일기 추가, 삭제, 수정 기능

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,6 +1,6 @@
 import { useRef, useState } from 'react';
 
-const DiaryEditor = () => {
+const DiaryEditor = ({ onCreate }) => {
 
     const authorInput = useRef();
     const contentInput = useRef();
@@ -11,9 +11,11 @@ const DiaryEditor = () => {
         emotion: 1,
     });
 
+    //내용이 입력될 때마다 호출되는 콜백함수. useRef
     const handleStateChange = (e) => {
         setState({
             ...state,
+            //이전에 저장돼있던 state 정보들을 spread 연산자로 뿌려준다.
             [e.target.name]: e.target.value,
         })
     };
@@ -27,11 +29,17 @@ const DiaryEditor = () => {
             contentInput.current.focus();
             return;
         }
+        onCreate(state.author, state.content, state.emotion); //데이터 추가
+        setState({ //작성 내용 초기화
+            author: "",
+            content: "",
+            emotion: 1,
+        })
         alert("저장되었습니다.");
     }
 
     return (
-        <form className="DiaryEditor">
+        <div className="DiaryEditor">
             <h2>오늘의 일기</h2>
             <div>
                 <input
@@ -48,7 +56,10 @@ const DiaryEditor = () => {
             </div>
             <div>
                 <span>오늘의 감정점수: </span>
-                <select name="emotion" value={state.emotion} onChange={handleStateChange}>
+                <select
+                    name="emotion"
+                    value={state.emotion}
+                    onChange={handleStateChange}>
                     <option value={1}>1</option>
                     <option value={2}>2</option>
                     <option value={3}>3</option>
@@ -57,10 +68,10 @@ const DiaryEditor = () => {
                 </select>
             </div>
             <div>
-                <button value="Submit"
+                <button
                     onClick={handleSubmit}>일기 저장하기</button>
             </div>
-        </form >
+        </div >
     )
 };
 
